88040713834d6f677ee39ba6404e42b336595d06
vsVikramSingh <vsvikramsingh5200@gmail.com>
2023-12-09 20:27:38 +0530

    update

---
+++ RegLogApplication/frontend/src/components/pages/register/Register.js
@@ -0,0 +1,128 @@
+// src/Register.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import './register.css'
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    email: '',
+    age: '',
+    gender: '',
+    dateofbirth: '',
+    maritalstatus: '',
+    fathername: '',
+    mothername: '',
+    city: '',
+    state: '',
+    country: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('http://your-api-url/api/register', formData);
+      console.log('User registered successfully');
+    } catch (error) {
+      console.error('Registration failed:', error.response?.data?.error || 'Unknown error');
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Registration Form</h2>
+      <form onSubmit={handleSubmit} className="register-form">
+        <div className="form-group">
+          <label>Username:</label>
+          <input type="text" name="username" value={formData.username} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+          <label>Password:</label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+          <label>Email:</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+          <label>Age:</label>
+          <input type="number" name="age" value={formData.age} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+          <label>Gender:</label>
+          <div>
+            <label>
+              <input type="radio" name="gender" value="male" checked={formData.gender === 'male'} onChange={handleChange} />Male
+            </label>
+            <label>
+              <input type="radio" name="gender" value="female" checked={formData.gender === 'female'} onChange={handleChange} />Female
+            </label>
+            <label>
+              <input type="radio" name="gender" value="other" checked={formData.gender === 'other'} onChange={handleChange} />Other
+            </label>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>Date of Birth:</label>
+          <input type="date" name="dateofbirth" value={formData.dateofbirth} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+        <label>Marital Status:</label>
+          <div>
+            <label>
+              <input type="radio" name="maritalstatus" value="single" checked={formData.maritalstatus === 'single'} onChange={handleChange} />Single
+            </label>
+            <label>
+              <input type="radio" name="maritalstatus" value="married" checked={formData.maritalstatus === 'married'} onChange={handleChange} />Married
+            </label>
+            <label>
+              <input type="radio" name="maritalstatus" value="divorced" checked={formData.maritalstatus === 'divorced'} onChange={handleChange} />Divorced
+            </label>
+            </div>
+        </div>
+
+        <div className="form-group">
+          <label>Father's Name:</label>
+          <input type="text" name="fathername" value={formData.fathername} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+          <label>Mother's Name:</label>
+          <input type="text" name="mothername" value={formData.mothername} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+          <label>City:</label>
+          <input type="text" name="city" value={formData.city} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+          <label>State:</label>
+          <input type="text" name="state" value={formData.state} onChange={handleChange} required />
+        </div>
+
+        <div className="form-group">
+          <label>Country:</label>
+          <input type="text" name="country" value={formData.country} onChange={handleChange} required />
+        </div>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
